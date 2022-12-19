bdfa5697bcf37cc1ccd76102fd5fd33173f9653e
PeterseerZhang <peterseer@163.com>
2022-12-19 17:09:16 -0500

    gate progress

--- Assets/Teleportation Pad/JumpPad.cs
+++ Assets/Teleportation Pad/JumpPad.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     public float padCD = 2;
+    private bool padCooling;
+    private float padTimer;
     public bool padisUsing = false;
     public bool padisActive = true;
+    public float walkSpeed = 5;
+    public float RunSpeed = 10;
+    public float jumpSpeed = 8;
+    private CollisionFlags m_CollisionFlags;
+    private CharacterController m_CharacterController;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +25,61 @@ public class JumpPad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(padCooling)
+        {
+            if(padTimer < padCD)
+            {
+                padTimer += Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Pad Charged!");
+                padTimer = 0;
+                padCooling = false;
+            }
+        }
+    }
+
+     public void checkJump()
+    {
+        if(!padCooling)
+        {
+
+        }
+    }
+
+    public void goJump()
+    {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            print("´þµ½ÄãÁË");
+            GameObject player = other.gameObject;
+            Rigidbody body = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 Vector3myVector3 = player.transform.position;
+            body.AddForce(body.transform.up*10f,  ForceMode.Impulse);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        m_CharacterController = hit.gameObject.GetComponent<CharacterController>();
+        //dont move the rigidbody if the character is on top of it
+        if (m_CollisionFlags == CollisionFlags.Below)
+        {
+            return;
+        }
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        body.AddForceAtPosition(m_CharacterController.velocity * 1.5f, hit.point, ForceMode.Impulse);
+        print("Ah ha");
     }
 }

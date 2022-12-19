8482e742d94ba37ac52bc8a636f1720fa175ed2c
PeterseerZhang <peterseer@163.com>
2022-12-18 22:29:23 -0500

    entrance almost done, add pad

---
+++ Assets/Teleportation Pad/JumpPad.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float padCD = 2;
+    public bool padisUsing = false;
+    public bool padisActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
